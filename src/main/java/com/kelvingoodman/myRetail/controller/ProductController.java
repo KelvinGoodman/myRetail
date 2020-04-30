104d47dcdd620f1a08588c832900eb346a6c85bc
Kelvin Goodman <kelvin12goodman@gmail.com>
2020-04-29 20:21:02 -0400

    getting product name from red sky api

--- src/main/java/com/kelvingoodman/myRetail/controller/ProductController.java
+++ src/main/java/com/kelvingoodman/myRetail/controller/ProductController.java
@@ -1,17 +1,29 @@
-package com.kelvingoodman.myRetail.resource;
+package com.kelvingoodman.myRetail.controller;
 
+import com.kelvingoodman.myRetail.dto.Price;
 import com.kelvingoodman.myRetail.dto.Product;
-import com.kelvingoodman.myRetail.service.impl.RedSkyServiceImpl;
+import com.kelvingoodman.myRetail.service.RedSkyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ProductResource {
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
 
-    @GetMapping("/product")
-    public Product getProduct() {
-        RedSkyServiceImpl redSkyService = new RedSkyServiceImpl();
-        redSkyService.getProductInfo(1);
-        return new Product();
+@RestController
+public class ProductController {
+
+    @Autowired
+    RedSkyService redSkyService;
+
+    @GetMapping("/product/{id}")
+    public Product getProduct(@PathVariable("id") @NotBlank @Size(max = 10) int id) {
+        System.out.println(id);
+        return new Product()
+                .withId(id)
+                .withName(redSkyService.getProductInfo(id).getProduct().getItem().getProduct_description().getTitle())
+                .withCurrent_price(new Price().withCurrency_code("USD").withValue(BigDecimal.valueOf(100)));
     }
 }
